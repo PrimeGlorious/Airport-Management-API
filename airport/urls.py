718d121d4c21705ac14b5ad5b1559cd01a34ca4d
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-28 23:21:20 +0300

    add Cargo routers

--- airport/urls.py
+++ airport/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from airport.views import AirportViewSet
-
+from airport.views import AirportViewSet, CargoViewSet
 
 router = DefaultRouter()
 router.register("airports", AirportViewSet)
+router.register("cargos", CargoViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- airport/views.py
+++ airport/views.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets
 
-from airport.models import Airport
-from airport.serializers import AirportSerializer
+from airport.models import Airport, Cargo
+from airport.serializers import AirportSerializer, CargoSerializer, CargoListSerializer, CargoDetailSerializer
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+
+
+class CargoViewSet(viewsets.ModelViewSet):
+    queryset = Cargo.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CargoListSerializer
+        elif self.action == "retrieve":
+            return CargoDetailSerializer
+
+        return CargoSerializer
