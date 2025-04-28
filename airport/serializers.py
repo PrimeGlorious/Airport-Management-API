81634e532aa4c2f0c02e79357c12ae68bcc4b9a0
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-28 23:21:11 +0300

    create Cargo serializers

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from airport.models import Airport
+from airport.models import Airport, Cargo
 
 
 class AirportSerializer(serializers.ModelSerializer):
@@ -10,4 +10,47 @@ class AirportSerializer(serializers.ModelSerializer):
             "id",
             "name",
             "closest_big_city",
+        )
+
+
+class CargoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cargo
+        fields = (
+            "id",
+            "description",
+            "weight",
+            "volume"
+        )
+
+
+class CargoListSerializer(serializers.ModelSerializer):
+    shorted_description = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cargo
+        fields = (
+            "id",
+            "shorted_description",
+            "weight",
+            "volume",
+            "is_delivered",
+            "condition"
+        )
+
+    @staticmethod
+    def get_shorted_description(obj):
+        return obj.shorted_description
+
+
+class CargoDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cargo
+        fields = (
+            "id",
+            "description",
+            "weight",
+            "volume",
+            "is_delivered",
+            "condition"
         )
