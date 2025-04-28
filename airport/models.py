89beb5fea00f73398d69e534e4af163c47cd4241
PrimeGlorious <TheComboPunk@gmail.com>
2025-04-28 22:22:31 +0300

    create Cargo model

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -7,3 +8,19 @@ class Airport(models.Model):
 
     class Meta:
         unique_together = ("name", "closest_big_city")
+
+
+class Cargo(models.Model):
+    class ConditionChoices(models.TextChoices):
+        GOOD = "good", "Good"
+        DAMAGED = "damaged", "Damaged"
+
+    description = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.DecimalField(max_digits=10, decimal_places=2)
+    is_delivered = models.BooleanField(default=False)
+    condition = models.CharField(
+        max_length=7,
+        choices=ConditionChoices,
+        default=ConditionChoices.GOOD,
+    )
