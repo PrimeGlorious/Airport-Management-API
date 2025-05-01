264351253734ce2177be4ed6940e5baed6030349
PrimeGlorious <TheComboPunk@gmail.com>
2025-05-01 20:25:03 +0300

    add validation for registration_number

--- config/base/models.py
+++ config/base/models.py
@@ -1,5 +1,8 @@
+import re
+
 from django.core.validators import MinValueValidator
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from airport.models import Route
 
@@ -17,6 +20,16 @@ class BaseAirplane(models.Model):
 
     class Meta:
         abstract = True
+
+    def clean(self):
+        super().clean()
+
+        if not re.match(r"^[A-Za-z]{3}\d+$", self.registration_number):
+            raise ValidationError("Registration number must start with 3 letters followed by numbers.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.model} | {self.registration_number}"
